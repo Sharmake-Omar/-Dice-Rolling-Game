ad144c8eed90d44c5f434318cd2d8a89342edc09
Sharmake Omar <Sharmakabdinur@gmail.com>
2025-04-14 16:06:36 +0200

    change of the code

--- index.py
+++ index.py
@@ -1,12 +1,27 @@
 import random
+
+roll_count = 0  # Counter for number of rolls
+
 while True:
- choice = input('Roll the dice? (y/n): ').lower()
- if choice == 'y':
-    dice1 = random.randint(1, 6)
-    dice2 = random.randint(1,6)
-    print(f'({dice1}, {dice2})')
- elif choice == 'n':
-    print('Thanks for playing')
-    break
- else:
-    print('Invalid choice!')
+    choice = input('Roll the dice? (y/n): ').lower()
+
+    if choice == 'y':
+        try:
+            num_dice = int(input("How many dice do you want to roll? "))
+            if num_dice <= 0:
+                print("Please enter a positive number of dice.")
+                continue
+        except ValueError:
+            print("Invalid input! Please enter a number.")
+            continue
+
+        roll_count += 1
+        results = [random.randint(1, 6) for _ in range(num_dice)]
+        print(f'Roll {roll_count}: {tuple(results)}')
+
+    elif choice == 'n':
+        print(f'Thanks for playing! You rolled the dice {roll_count} times.')
+        break
+
+    else:
+        print('Invalid choice!')
